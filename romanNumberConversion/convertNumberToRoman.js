71b0ea2134e6b8cf78430882308c317d1c14aafd
Tania Mathern <tania.mathern@gmail.com>
2019-06-28 11:59:58 -0700

    Added Tests for the conversion function (currently directly in-file)

---
+++ romanNumberConversion/convertNumberToRoman.js
@@ -0,0 +1,83 @@
+// Created: June 28th 2019
+// Author: Tania Mathern
+// Last edit: June 28th 2019
+
+let lowLimit = 0;
+let upperLimit = 255;
+
+// About roman numerals: https://en.wikipedia.org/wiki/Roman_numerals
+// Roman "numbers" include: I, V, X, L, C, D, M
+function convertNumToRoman(num){
+    
+}
+
+
+// start only looking at functionality, tests in same file
+let assert = require('assert');
+
+// Checks that a test fails when it should,
+// and also that a test passes when it should
+
+describe('Basic number to roman conversion (0-255)', function () {
+    it('should convert 1 to I', function () {
+            assert.equal(convertNumToRoman(1), 'I');
+        });
+
+    it('should convert 5 to V', function () {
+            assert.equal(convertNumToRoman(5), 'V');
+        });
+
+    it('should convert 10 to X', function () {
+            assert.equal(convertNumToRoman(10), 'X');
+        });
+
+    it('should convert 50 to L', function () {
+            assert.equal(convertNumToRoman(50), 'L');
+        });
+
+    it('should convert 100 to C', function () {
+            assert.equal(convertNumToRoman(100), 'C');
+        });
+});
+
+describe('Special cases number to roman conversion (0-255)', function () {
+    it('should convert 4 to IV', function () {
+            assert.equal(convertNumToRoman(4), 'IV');
+        });
+
+    it('should convert 9 to IX', function () {
+            assert.equal(convertNumToRoman(9), 'IX');
+        });
+
+    it('should convert 40 to XL', function () {
+            assert.equal(convertNumToRoman(40), 'XL');
+        });
+
+    it('should convert 90 to XC', function () {
+            assert.equal(convertNumToRoman(90), 'XC');
+        });
+});
+
+describe('General number to roman conversion', function () {
+    it('should return 123 converted to CXIII', function () {
+            assert.equal(convertNumToRoman(123), 'CXXIII');
+        });
+    it('should return 149 converted to CXLIX', function () {
+            assert.equal(convertNumToRoman(149), 'CXLIX');
+        });
+    it('should return 243 converted to CCXLIII', function () {
+            assert.equal(convertNumToRoman(243), 'CCXLIII');
+        });
+    it('should return 244 converted to CCXLIV', function () {
+            assert.equal(convertNumToRoman(244), 'CCXLIV');
+        });
+    it('should return 249 converted to CCXLIV', function () {
+            assert.equal(convertNumToRoman(249), 'CCXLIX');
+        });
+    it('should return 255 converted to CCLV', function () {
+            assert.equal(convertNumToRoman(255), 'CCLV');
+        });
+    it('should return 8 converted to VIII', function () {
+            assert.equal(convertNumToRoman(8), 'VIII');
+        });
+});
