a481f59952d9f27c062cc359e6027a2224cb7285
Tania Mathern <tania.mathern@gmail.com>
2019-06-28 17:50:13 -0700

    Positive API tests

--- tests/convertNumberToRomanAPITest.js
+++ tests/convertNumberToRomanAPITest.js
@@ -1,6 +1,6 @@
 var request = require('supertest');
 
-describe('Loading express', function () {
+describe('Responds to valid routes', function () {
     var server;
 
     beforeEach(function () {
@@ -11,9 +11,115 @@ describe('Loading express', function () {
         server.close();
     });
 
-    it('responds to /health', function testSlash(done) {
+    it('responds to /romannumeral (query = 50)', function testSlash(done) {
         request(server)
-            .get('/health')
-            .expect(200, done);
+            .get('/romannumeral')
+            .query({ query: 50 })
+            .set('Accept', 'application/json')
+            .expect(200, {
+                    'roman': 'L'
+                },
+            done);
+    });
+
+    it('responds to /romannumeral (query = 149)', function testSlash(done) {
+        request(server)
+            .get('/romannumeral')
+            .query({ query: 149 })
+            .set('Accept', 'application/json')
+            .expect(200, {
+                    'roman': 'CXLIX'
+                },
+            done);
+    });
+
+    it('responds to /romannumeral (query = 249)', function testSlash(done) {
+        request(server)
+            .get('/romannumeral')
+            .query({ query: 249 })
+            .set('Accept', 'application/json')
+            .expect(200, {
+                    'roman': 'CCXLIX'
+                },
+            done);
+    });
+
+    it('responds to /romannumeral (query = 1606)', function testSlash(done) {
+        request(server)
+            .get('/romannumeral')
+            .query({ query: 1606 })
+            .set('Accept', 'application/json')
+            .expect(200, {
+                    'roman': 'MDCVI'
+                },
+            done);
+    });
+
+    it('responds to /romannumeral (query = 3999)', function testSlash(done) {
+        request(server)
+            .get('/romannumeral')
+            .query({ query: 3999 })
+            .set('Accept', 'application/json')
+            .expect(200, {
+                    'roman': 'MMMCMXCIX'
+                },
+            done);
+    });
+
+    it('responds to /romannumeral (query = 2200000000)', function testSlash(done) {
+        request(server)
+            .get('/romannumeral')
+            .query({ query: 2200000000 })
+            .set('Accept', 'application/json')
+            .expect(200, {
+                    'roman': 'M\u0305\u0305M\u0305\u0305C\u0305\u0305C\u0305\u0305'
+                },
+            done);
+    });
+
+    it('responds to /romannumeral (query = 400000000)', function testSlash(done) {
+        request(server)
+            .get('/romannumeral')
+            .query({ query: 400000000 })
+            .set('Accept', 'application/json')
+            .expect(200, {
+                    'roman': 'C\u0305\u0305D\u0305\u0305'
+                },
+            done);
+    });
+
+    it('responds to /romannumeral (query = 1900400003)', function testSlash(done) {
+        request(server)
+            .get('/romannumeral')
+            .query({ query: 1900400003 })
+            .set('Accept', 'application/json')
+            .expect(200, {
+                    'roman': 'M\u0305\u0305C\u0305\u0305M\u0305\u0305C\u0305D\u0305III'
+                },
+            done);
     });
 });
+
+/*describe('Responds to non existing routes with 404', function () {
+    var server;
+
+    beforeEach(function () {
+        server = require('./../server.js');
+    });
+
+    afterEach(function () {
+        server.close();
+    });
+});
+
+describe('Responds to invalid routes with error', function () {
+    var server;
+
+    beforeEach(function () {
+        server = require('./../server.js');
+    });
+
+    afterEach(function () {
+        server.close();
+    });
+});*/
