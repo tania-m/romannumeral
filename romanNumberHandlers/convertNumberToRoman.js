4b1c42b18d794b69e53f64ede32b12f2a1cc5320
Tania Mathern <tania.mathern@gmail.com>
2019-06-30 16:21:58 -0700

    Refactored decimal to roman map creation logic

--- romanNumberHandlers/convertNumberToRoman.js
+++ romanNumberHandlers/convertNumberToRoman.js
@@ -15,7 +15,7 @@ class NumberToRomanConverter {
         this.lowLimit = 0;
         this.upperLimit = 2200000000; // smaller than       
 
-        this.decimalToRomanMap = this._buildNumberToRomanMap();
+        this.romanMap = this._buildNumberToRomanMap();
     }
 
     /**
@@ -24,17 +24,6 @@ class NumberToRomanConverter {
      */
     _buildNumberToRomanMap(){
         let romanMap = new Map(); 
-
-        // We need the equivalent to romans characters for 1 to 1000:
-        // C,L,X,V,I : 100, 50, 10, 5, 1 to built numbers;
-        // but also some exceptions because of how roman notation works
-        // (XC is 100-10=90, CX is 100+10=110)
-        // XC, XL, IX, IV: 90, 40, 9, 4.
-        // This is needed because roman numbers are position-dependent
-
-        // We can build all numbers up to **2 200 000 000** using this "alphabet"
-        // so we map the "alphabets":
-
         /* 
             **Note**: Here we have a limit in size, so the look-up table is good:
             We don't need to reconstruct the key roman numerals for large numbers.
@@ -43,50 +32,92 @@ class NumberToRomanConverter {
             large numbers follow the notation rule for numbers between 1 and 1000
             with additional bars on top depending on the size.
         */
-        romanMap.set(1000000000, 'M\u0305\u0305');
+        
+        if(this.upperLimit >= 4000){ // add mapping for large numbers
+            romanMap = this._buildNumberToRomanMapForLargeNumbers(romanMap);
+            romanMap = this._buildNumberToRomanMapForMidsizeNumbers(romanMap);
+        }
 
-        romanMap.set(900000000, 'C\u0305\u0305M\u0305\u0305');
-        romanMap.set(500000000, 'D\u0305\u0305');
-        romanMap.set(400000000, 'C\u0305\u0305D\u0305\u0305');
-        romanMap.set(100000000, 'C\u0305\u0305');
-
-        romanMap.set(90000000, 'X\u0305\u0305C\u0305\u0305');
-        romanMap.set(50000000, 'L\u0305\u0305');
-        romanMap.set(40000000, 'X\u0305\u0305L\u0305\u0305');
-        romanMap.set(10000000, 'X\u0305\u0305');
-
-        romanMap.set(9000000, 'I\u0305\u0305X\u0305\u0305');
-        romanMap.set(5000000, 'V\u0305\u0305');
-        romanMap.set(4000000, 'I\u0305\u0305V\u0305\u0305');
-        romanMap.set(1000000, 'M\u0305');
-
-        romanMap.set(900000, 'C\u0305M\u0305');
-        romanMap.set(500000, 'D\u0305');
-        romanMap.set(400000, 'C\u0305D\u0305');
-        romanMap.set(100000, 'C\u0305');
-        romanMap.set(90000, 'X\u0305C\u0305');
-        romanMap.set(50000, 'L\u0305');
-        romanMap.set(40000, 'X\u0305L\u0305');
-        romanMap.set(10000, 'X\u0305');
-        romanMap.set(9000, 'I\u0305X\u0305');
-        romanMap.set(5000, 'V\u0305');
-        romanMap.set(4000, 'I\u0305V\u0305');
-
-        romanMap.set(1000, 'M');
-        romanMap.set(900, 'CM');
-        romanMap.set(500, 'D');
-        romanMap.set(400, 'CD');
-        romanMap.set(100, 'C');
-        romanMap.set(90, 'XC');
-        romanMap.set(50, 'L');
-        romanMap.set(40, 'XL');
-        romanMap.set(10, 'X');
-        romanMap.set(9, 'IX');
-        romanMap.set(5, 'V');
-        romanMap.set(4, 'IV');
-        romanMap.set(1, 'I');
+        
+        if(this.upperLimit >= 0){ // "base" mappings
+            romanMap = this._buildNumberToRomanMapForSmallNumbers(romanMap);
+        }        
 
         return romanMap;
+    }
+
+    /** Builds the map from decimal to romans for numbers with key values from 1000000 to 1000000000. 
+     * Not all decimal numbers need a representation, a predefined set of values can help built a lot of others.
+     * @param {Map} decimalToRomanMap, a map to contain decimal to roman mapping
+     * @returns {Map} containing the additional mappings for large numbers
+    */
+    _buildNumberToRomanMapForLargeNumbers(decimalToRomanMap){
+        decimalToRomanMap.set(1000000000, 'M\u0305\u0305');
+
+        decimalToRomanMap.set(900000000, 'C\u0305\u0305M\u0305\u0305');
+        decimalToRomanMap.set(500000000, 'D\u0305\u0305');
+        decimalToRomanMap.set(400000000, 'C\u0305\u0305D\u0305\u0305');
+        decimalToRomanMap.set(100000000, 'C\u0305\u0305');
+
+        decimalToRomanMap.set(90000000, 'X\u0305\u0305C\u0305\u0305');
+        decimalToRomanMap.set(50000000, 'L\u0305\u0305');
+        decimalToRomanMap.set(40000000, 'X\u0305\u0305L\u0305\u0305');
+        decimalToRomanMap.set(10000000, 'X\u0305\u0305');
+
+        decimalToRomanMap.set(9000000, 'I\u0305\u0305X\u0305\u0305');
+        decimalToRomanMap.set(5000000, 'V\u0305\u0305');
+        decimalToRomanMap.set(4000000, 'I\u0305\u0305V\u0305\u0305');
+        decimalToRomanMap.set(1000000, 'M\u0305');
+
+        return decimalToRomanMap;
+    }
+
+    /** Builds the map from decimal to romans for numbers with key values from 4000 to 90000. 
+     * Not all decimal numbers need a representation, a predefined set of values can help built a lot of others.
+     * @param {Map} decimalToRomanMap, a map to contain decimal to roman mapping
+     * @returns {Map} containing additional the mappings for midsize numbers
+    */
+    _buildNumberToRomanMapForMidsizeNumbers(decimalToRomanMap){
+        decimalToRomanMap.set(900000, 'C\u0305M\u0305');
+        decimalToRomanMap.set(500000, 'D\u0305');
+        decimalToRomanMap.set(400000, 'C\u0305D\u0305');
+        decimalToRomanMap.set(100000, 'C\u0305');
+
+        decimalToRomanMap.set(90000, 'X\u0305C\u0305');
+        decimalToRomanMap.set(50000, 'L\u0305');
+        decimalToRomanMap.set(40000, 'X\u0305L\u0305');
+        decimalToRomanMap.set(10000, 'X\u0305');
+
+        decimalToRomanMap.set(9000, 'I\u0305X\u0305');
+        decimalToRomanMap.set(5000, 'V\u0305');
+        decimalToRomanMap.set(4000, 'I\u0305V\u0305');
+
+        return decimalToRomanMap;
+    }
+
+    /** Builds the map from decimal to romans with key values for numbers from 1 to 1000.
+     *  Not all decimal numbers need a representation, a predefined set of values can help built a lot of others.
+     *  0 is excluded because roman numbers don't have a 0.      * 
+     * @param {Map} decimalToRomanMap, a map to contain decimal to roman mapping
+     * @returns {Map} containing the mappings
+    */
+    _buildNumberToRomanMapForSmallNumbers(decimalToRomanMap){
+
+        decimalToRomanMap.set(1000, 'M');
+        decimalToRomanMap.set(900, 'CM');
+        decimalToRomanMap.set(500, 'D');
+        decimalToRomanMap.set(400, 'CD');
+        decimalToRomanMap.set(100, 'C');
+        decimalToRomanMap.set(90, 'XC');
+        decimalToRomanMap.set(50, 'L');
+        decimalToRomanMap.set(40, 'XL');
+        decimalToRomanMap.set(10, 'X');
+        decimalToRomanMap.set(9, 'IX');
+        decimalToRomanMap.set(5, 'V');
+        decimalToRomanMap.set(4, 'IV');
+        decimalToRomanMap.set(1, 'I');
+
+        return decimalToRomanMap;
     }
 
     /**
@@ -113,11 +144,9 @@ class NumberToRomanConverter {
         }
     
 
-        // now we are sure we have a number we can handle
-        // Result will hold the converted value
+        // now we are sure we have a number we can handle. Result will hold the converted value
         let result = '';
-        
-        for (let [decimal, roman] of this.decimalToRomanMap) { // keys in a map are kept in order of addition
+        for (let [decimal, roman] of this.romanMap) { // keys in a map are kept in order of addition
             // so we have the key in the sorted order we need, aka from largest to smallest
             
             // roman number will be build from left to right
